f934850c95017e2dc9e3d4e72933943ca68bff5f
zhouweihua <1040052214@qq.com>
2021-07-29 14:47:48 +0800

    deploy

---
+++ service-worker.js
@@ -0,0 +1,144 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "404.html",
+    "revision": "f6d7c9e746631bd2bd7f27390010ea1e"
+  },
+  {
+    "url": "accumulate/index.html",
+    "revision": "6526fc280391e4e8895379ec8908fd63"
+  },
+  {
+    "url": "assets/css/0.styles.6aaa91c1.css",
+    "revision": "fde2c425d4e8a0f87d70ece6f5dd13a3"
+  },
+  {
+    "url": "assets/img/search.83621669.svg",
+    "revision": "83621669651b9a3d4bf64d1a670ad856"
+  },
+  {
+    "url": "assets/js/2.858e80da.js",
+    "revision": "59b9e09855a16d382db7d8ff58d74b0d"
+  },
+  {
+    "url": "assets/js/3.dad9bf50.js",
+    "revision": "79d535a9dd7f81156e73c93b63860feb"
+  },
+  {
+    "url": "assets/js/4.ab31d681.js",
+    "revision": "18cf410483c45bf839135a74c9939f2a"
+  },
+  {
+    "url": "assets/js/5.421abe45.js",
+    "revision": "58bcabc909625e158bf1aecbf72abd8a"
+  },
+  {
+    "url": "assets/js/6.1ebc0ca8.js",
+    "revision": "13edb4286a68a885785d2a560a9bc3bf"
+  },
+  {
+    "url": "assets/js/7.1180ccc3.js",
+    "revision": "88bdf9974790814db583e9304a50f330"
+  },
+  {
+    "url": "assets/js/app.989f1945.js",
+    "revision": "79c1ce7a16f7b2e02a8b873db6846d68"
+  },
+  {
+    "url": "guide.html",
+    "revision": "ef000ed6c7d08aafb4325231f7b3309f"
+  },
+  {
+    "url": "images/eg1.png",
+    "revision": "b6e00451aa6e2fb07803babc3be44fe3"
+  },
+  {
+    "url": "images/eg12.png",
+    "revision": "716c3bede731ed6eecb026377f31aac1"
+  },
+  {
+    "url": "images/eg13.png",
+    "revision": "022c1e3380b12aa21dd816cd4f1aeaae"
+  },
+  {
+    "url": "images/eg14.png",
+    "revision": "c0b03bb9c818398235ae5ac24fe91204"
+  },
+  {
+    "url": "images/eg2.png",
+    "revision": "1c31890ab04672b5b9daad1f8216a89b"
+  },
+  {
+    "url": "images/eg3.png",
+    "revision": "c3145209954ef832a2c62740a2133a8b"
+  },
+  {
+    "url": "images/eg4.png",
+    "revision": "7b4a9dd311c20e60f1f67205845deca1"
+  },
+  {
+    "url": "images/eg5.png",
+    "revision": "d70a372965f420d2faaf40259dce9a1c"
+  },
+  {
+    "url": "images/eg6.png",
+    "revision": "64c96504fe466f4baa99423a71573892"
+  },
+  {
+    "url": "images/eg7.png",
+    "revision": "5814399d3ba9bdcedb588e2b65059e67"
+  },
+  {
+    "url": "images/eg8.png",
+    "revision": "cd30b75ad80917829755a2693a374b02"
+  },
+  {
+    "url": "images/icon.png",
+    "revision": "276978e6f49032f3b5f025e852fdc53d"
+  },
+  {
+    "url": "images/photo.jpg",
+    "revision": "d4d77052d44bea42bbfc4dba196bde93"
+  },
+  {
+    "url": "index.html",
+    "revision": "45e14912ac40cfa86abb07c8540eb14a"
+  },
+  {
+    "url": "others/index.html",
+    "revision": "0d639f5af879137c82967b7ff88fa695"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+addEventListener('message', event => {
+  const replyPort = event.ports[0]
+  const message = event.data
+  if (replyPort && message && message.type === 'skip-waiting') {
+    event.waitUntil(
+      self.skipWaiting().then(
+        () => replyPort.postMessage({ error: null }),
+        error => replyPort.postMessage({ error })
+      )
+    )
+  }
+})
